f1fd6aed0410d105a5940e9cf718630004463229
WAqar Abid <waqar.abid2003@gmail.com>
2024-08-21 16:56:09 +0500

    calculator react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const Click = (value) => {
+    setInput(input + value);
+  };
+  
+  
+  const calculateResult = () => {
+    setInput(eval(input));
+  };
+
+  const clearInput = () => {
+    setInput("");
+  };
+
+  // const name = () => {
+  //   setInput ("Calc")
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test">
+      <div className="card">
+        <div className="display">
+          <input
+            type="text"
+            value={input}
+            style={{ width: "78%" }}
+            readOnly
+            placeholder="0"
+          />
+        </div>
+
+        <div className="button">
+          <button className="C" onClick={clearInput}>
+            C
+          </button>
+          <button className="operations" onClick={() => Click("/")}>/</button>
+          <button className="operations"onClick={() => Click("*")}>*</button>
+          <button className="operations"onClick={() => Click("-")}>-</button>
+        </div>
+
+        <div className="button1">
+          <button onClick={() => Click("7")}>7</button>
+          <button onClick={() => Click("8")}>8</button>
+          <button onClick={() => Click("9")}>9</button>
+          <button className="operations"onClick={() => Click("+")}>+</button>
+        </div>
+
+        <div className="button2">
+          <button onClick={() => Click("4")}>4</button>
+          <button onClick={() => Click("5")}>5</button>
+          <button onClick={() => Click("6")}>6</button>
+          <button className="operations" onClick={() => Click("0")}>0</button>
+        </div>
+
+        <div className="button3">
+          <button onClick={() => Click("1")}>1</button>
+          <button onClick={() => Click("2")}>2</button>
+          <button onClick={() => Click("3")}>3</button>
+          <button className="point" onClick={() => Click(".")}>
+            .
+          </button>
+
+          
+          
+        </div>
+        <div className="Equal">
+          <button className="but" onClick={calculateResult}>
+            =
+          </button>
+        </div>
+        {/* <div>
+        <button  className="name"onClick={name}></button>
+
+       </div>    */}
+      </div>
     </div>
   );
 }
